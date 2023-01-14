7220cf29f298ca4708175f5b65919de44583f6bb
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2023-01-14 13:42:42 +0000

    Setup HTTPS server Fix error handling from async routes

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,23 @@ dotenv.config();
 
 import { client } from "./authClient";
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { search, extractLyrics } from "./genius";
+import https from "https";
+import fs from "fs";
+import asyncHandler from "express-async-handler";
+
+const options = {
+    key: fs.readFileSync("./keys/localhost-key.pem"),
+    cert: fs.readFileSync("./keys/localhost.pem"),
+};
 
 const app = express();
+
+const server = https.createServer(options, app);
+
 const port = 3000;
-const URL = `http://localhost:${port}`;
+const URL = `https://localhost:${port}`;
 
 const redirectPath = "/auth/callback";
 const redirectUri = `${URL}${redirectPath}`;
@@ -17,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const authorizationUrl = client.authorizeURL({
-    redirect_uri: "http://localhost:3000/auth/callback",
+    redirect_uri: redirectUri,
 });
 
 app.get("/", (req, res) => {
@@ -29,34 +40,40 @@ app.get("/auth", (req, res) => {
     res.redirect(authorizationUrl);
 });
 
-app.get(redirectPath, async (req, res) => {
-    const { code } = req.query;
+app.get(
+    redirectPath,
+    asyncHandler(async (req, res) => {
+        const { code } = req.query;
 
-    const options = {
-        code: code as string,
-        redirect_uri: redirectUri,
-    };
+        const options = {
+            code: code as string,
+            redirect_uri: redirectUri,
+        };
 
-    const token = await client.getToken(options);
+        const token = await client.getToken(options);
 
-    res.status(200).json(token);
-});
+        res.status(200).json(token.token.access_token);
+    })
+);
 
-app.post("/lyrics", async (req, res) => {
-    const { q, access_token } = req.body;
+app.post(
+    "/lyrics",
+    asyncHandler(async (req, res) => {
+        const { q, access_token } = req.body;
 
-    const song = await search(access_token, q);
+        const song = await search(access_token, q);
 
-    // Parse url for lyrics with cheerio
-    const lyrics = await extractLyrics(song.url);
+        // Parse url for lyrics with cheerio
+        const lyrics = await extractLyrics(song.url);
 
-    res.status(200).json(lyrics);
-});
+        res.status(200).json(lyrics);
+    })
+);
 
-app.use((err: Error, req: Request, res: Response) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     return console.log(`Express is listening at ${URL}`);
 });
